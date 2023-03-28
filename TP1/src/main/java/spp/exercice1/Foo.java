7cfc81e1c4dd0c9816315cd9994f4f00784a0b4d
Slonev0 <enzo.voisin@etudiant.univ-rennes1.fr>
2023-03-28 09:47:09 +0200

    Add comment to TP1

--- TP1/src/main/java/spp/exercice1/Foo.java
+++ TP1/src/main/java/spp/exercice1/Foo.java
@@ -1,18 +1,45 @@
 package spp.exercice1;
 
+/**
+ * Classe Foo, implémentation de SemaphoreInterface
+ */
 public class Foo implements SemaphoreInterface {
+    /**
+     * Nombre de jetons disponibles
+     */
     private int count = 0;
+
+    /**
+     * Nombre de threads en attente
+     */
     private int waiting = 0;
+
+    /**
+     * Constructeur de Foo par défaut, demandé dans le sujet
+     */
     public Foo(){}
+
+    /**
+     * Constructeur de Foo avec un nombre de jetons initial
+     * @param nbPermit Nombre de jetons initial
+     */
     public Foo(int nbPermit){
         count=nbPermit;
     }
+
+    /**
+     * Méthode up, ajoute un jeton et réveille un thread en attente
+     */
     @Override
     public synchronized void up() {
         count++;
         notify();
     }
 
+    /**
+     * Méthode down, retire un jeton, met le thread en attente s'il n'y a plus de jeton
+     * @throws InterruptedException
+     */
     @Override
     public synchronized void down() throws InterruptedException {
         if(count <= 0){
@@ -22,6 +49,10 @@ public class Foo implements SemaphoreInterface {
         count--;
     }
 
+    /**
+     * Méthode releaseAll, réveille tous les threads en attente
+     * @return Nombre de threads réveillés
+     */
     @Override
     public synchronized int releaseAll() {
         int nbThread = waiting;
