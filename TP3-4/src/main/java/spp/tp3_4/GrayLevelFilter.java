0c984b20c54728bcc0df30804c1a94572da56659
ghost-hikaru <melvin.mathurin35@gmail.com>
2023-04-04 19:55:56 +0200

    add greyFilter and begin the second filter

--- TP3-4/src/main/java/spp/tp3_4/GrayLevelFilter.java
+++ TP3-4/src/main/java/spp/tp3_4/GrayLevelFilter.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class GrayLevelFilter implements IFilter {
-    private final int margin;
+    private int margin;
 
     public GrayLevelFilter() {
         this.margin = 0;
@@ -14,14 +14,19 @@ public class GrayLevelFilter implements IFilter {
         return margin;
     }
 
+    public void setMargin(int margin){
+        this.margin = margin;
+    }
+
     @Override
     public void applyFilterAtPoint(int x, int y, BufferedImage imgIn, BufferedImage imgOut) {
         Color c = new Color(imgIn.getRGB(x, y));
         int r = c.getRed();
         int g = c.getGreen();
         int b = c.getBlue();
-        int gray = (int) (0.299 * r + 0.587 * g + 0.114 * b); // grayscale formula
+        //int gray = (int) (0.299 * r + 0.587 * g + 0.114 * b); // grayscale formula
+        int gray = (int) (0.3 * r + 0.3 * g + 0.3 * b);
         Color newColor = new Color(gray, gray, gray);
-        imgOut.setRGB(x, y, newColor.getRGB());
+        imgOut.setRGB(x - getMargin(), y - getMargin(), newColor.getRGB());
     }
 }
